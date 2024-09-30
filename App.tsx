7e68ebf7ea9977571d829b048a7761ff6ee23c46
Markus <markus147@live.se>
2024-09-30 13:30:46 +0200

    Prettier write

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
 import RootStackNavigator from "./navigators/RootStackNavigator";
 
 export default function App() {
